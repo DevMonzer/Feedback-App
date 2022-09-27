4c40b56fbfa7f9a6596f26974ee884142bf883c6
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-27 11:03:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
 
 import Header from './components/Header'
 
@@ -13,6 +15,8 @@ const App = () => {
             path='/'
             element={
               <>
+                <FeedbackForm />
+                <FeedbackStats />
                 <FeedbackList />
               </>
             }
