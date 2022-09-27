971a1bed1a4e57bcac1c9eaa2ea772ed7e8bb82c
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-27 10:55:28 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
-const Header = () => {
-  return <div>Header</div>
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
+
+  return (
+    <header style={headerStyles}>
+      <div className='container'>
+        <Link to='/' style={{ textDecoration: 'none', color: '#ff6a95' }}>
+          <h2>{text}</h2>
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
