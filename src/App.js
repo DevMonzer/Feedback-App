10ad91e001c90ffe000656fae6dddeb356034d96
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-27 10:56:11 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 
 const App = () => {
   return <div>App</div>
