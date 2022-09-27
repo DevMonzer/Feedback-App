1a9b70122bf06ace2e0df5beb2bca43da4d571fc
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-27 11:06:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutIconLink from './components/AboutIconLink'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
-
 import Header from './components/Header'
+import { FeedbackProvider } from './context/FeedbackContext'
+import AboutPage from './pages/AboutPage'
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Header />
-      <div className='container'>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <FeedbackForm />
-                <FeedbackStats />
-                <FeedbackList />
-              </>
-            }
-          ></Route>
-        </Routes>
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 
