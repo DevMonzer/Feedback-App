5c2fc999cd7ba92511ca752647aa355660facbc3
Monzer Mohammad <dev.monzer@gmail.com>
2022-09-27 10:59:53 +0300

    Update FeedbackItem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useContext } from 'react'
+import { FaEdit, FaTimes } from 'react-icons/fa'
+import FeedbackContext from '../context/FeedbackContext'
+import Card from './shared/Card'
 
-const FeedbackItem = () => {
-  return <div>FeedbackItem</div>
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+
+  return (
+    <Card>
+      <div className='num-display'>{item.rating}</div>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
+      <button onClick={() => editFeedback(item)} className='edit'>
+        <FaEdit color='purple' />
+      </button>
+      <div className='text-display'>{item.text}</div>
+    </Card>
+  )
 }
 
 export default FeedbackItem
